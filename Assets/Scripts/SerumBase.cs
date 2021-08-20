5189a7d0b1a7c0d901d7364718acd52672ea2aeb
wildhevire <wildhevire404@gmail.com>
2021-08-21 00:58:04 +0700

    serum time adjustment

---
+++ Assets/Scripts/SerumBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerumBase : MonoBehaviour
+{
+    [Header("Timer")]
+    [SerializeField] public float duration = 10f;
+    [SerializeField] public float timeRemaining = 10f;
+    [SerializeField] public bool timeIsRunning = false;
+}
