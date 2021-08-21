9986282e6e65135c3a8bcafb6f7e0f8eb581f24c
Milzan Malik <milzan_malik@outlook.com>
2021-08-21 18:18:00 +0700

    pause screen

---
+++ Assets/Scripts/UI/PauseController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private bool pauseState;
+    [SerializeField] private Animator Overlay;
+    void Start()
+    {
+
+    }
+
+    public void PauseBtn()
+    {
+        Overlay.SetTrigger("StartPause");
+        pauseState = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (pauseState)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Overlay.SetTrigger("StopPause");
+                pauseState = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Overlay.SetTrigger("StartPause");
+                pauseState = true;
+            }
+        }
+
+
+
+
+    }
+}
