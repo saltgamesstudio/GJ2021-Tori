25077c3a2023a2b541a837b1fe414639c80e01df
Milzan Malik <milzan_malik@outlook.com>
2021-08-22 01:07:19 +0700

    pass pause ui

--- Assets/Scripts/UI/SceneController.cs
+++ Assets/Scripts/UI/SceneController.cs
@@ -7,16 +7,19 @@ public class SceneController : MonoBehaviour
 {
     [SerializeField] private Animator transition;
     [SerializeField] private float transitionTime=1f;
+    [SerializeField] private GameObject PauseUI;
     public void PindahScene(string scene)
     {
+        var ui = Instantiate(PauseUI, new Vector3(0, 0, 0), Quaternion.identity);
+        DontDestroyOnLoad(ui);
         StartCoroutine(LoadScene(scene));
     }
 
-    IEnumerator LoadScene(string scene)
+    IEnumerator LoadScene(string m_scene)
     {
         transition.SetTrigger("Start");
         yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(m_scene);
     }
 
 
