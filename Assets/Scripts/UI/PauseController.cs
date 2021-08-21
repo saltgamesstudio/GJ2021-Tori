edbbfe38f7c059247604df3b0ef8bf8718e5b2f0
Milzan Malik <milzan_malik@outlook.com>
2021-08-22 00:25:34 +0700

    ui pause

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PauseController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private bool pauseState;
     [SerializeField] private Animator Overlay;
+    [SerializeField] private GameObject buttonPause;
+    [SerializeField] private Button buttonESC;
+    [SerializeField] private Button buttonR;
+
     void Start()
     {
 
@@ -30,14 +35,23 @@ public class PauseController : MonoBehaviour
             {
                 Overlay.SetTrigger("StopPause");
                 pauseState = false;
+                buttonESC.enabled = true;
+                buttonR.enabled = true;
+                EventSystem.current.SetSelectedGameObject(null);
+
             }
         }
         else
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
+                
                 Overlay.SetTrigger("StartPause");
                 pauseState = true;
+                buttonESC.enabled = false;
+                buttonR.enabled = false;
+                EventSystem.current.SetSelectedGameObject(buttonPause);
+
             }
         }
 
