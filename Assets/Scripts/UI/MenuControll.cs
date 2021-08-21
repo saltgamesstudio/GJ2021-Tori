73f51f3a45aab0962228d1c683033bf73be5d5a4
Milzan Malik <milzan_malik@outlook.com>
2021-08-21 15:11:14 +0700

    ui animation, button ripple, scene crossfade transition

---
+++ Assets/Scripts/UI/MenuControll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuControll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private bool menuState;
+    [SerializeField] private Animator OverlayMenu;
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!menuState)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                OverlayMenu.SetTrigger("OpenOverlay");
+                menuState = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                OverlayMenu.SetTrigger("CloseOverlay");
+                menuState = false;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/UI/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime=1f;
+    public void PindahScene(string scene)
+    {
+        StartCoroutine(LoadScene(scene));
+    }
+
+    IEnumerator LoadScene(string scene)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scene);
+    }
+
+
+}
