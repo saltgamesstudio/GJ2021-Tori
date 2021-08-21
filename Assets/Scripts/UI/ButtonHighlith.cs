1a4fff88002b4c9a55570c657bcaf516efc51ef4
Milzan Malik <milzan_malik@outlook.com>
2021-08-21 23:17:51 +0700

    UI Menu

---
+++ Assets/Scripts/UI/ButtonHighlith.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHighlith : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public Text theText;
+    private Color defautlColor;
+
+    private void Awake()
+    {
+        defautlColor = theText.color;
+    }
+    void Start()
+    {
+
+    }
+    void ISelectHandler.OnSelect(BaseEventData eventData)
+    {
+        theText.color = Color.white;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        theText.color = defautlColor;
+    }
+}
