914c2a54f0f56078e421a55bb341caa263da3fd7
Milzan Malik <milzan_malik@outlook.com>
2021-08-22 23:03:27 +0700

    death screen

---
+++ Assets/Scripts/UI/DeathScreenController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreenController : MonoBehaviour
+{
+    private Animator deathanim;
+    private bool isDead;
+
+    private void Start()
+    {
+        deathanim = GetComponent<Animator>();
+    }
+    public void PlayDeathScreen()
+    {
+        deathanim.SetTrigger("StartAnim");
+        isDead = true;
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("MainMenu");
+                isDead = false;
+            }
+        }
+        
+    }
+
+}
